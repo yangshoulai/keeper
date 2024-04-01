eac60ed87ed15e8d3dca743380b3e1c8c0108d82
yangshoulai <shoulai.yang@gmail.com>
2024-04-01 09:21:42 +0800

    Initial Commit

---
+++ Keeper2/Keeper2App.swift
@@ -0,0 +1,17 @@
+//
+//  Keeper2App.swift
+//  Keeper2
+//
+//  Created by 杨寿来 on 2024/4/1.
+//
+
+import SwiftUI
+
+@main
+struct Keeper2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
